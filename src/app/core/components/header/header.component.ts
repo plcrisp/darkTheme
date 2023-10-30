4b975c90e8bd06276c8d300d58308d2f4f000f1c
plcrisp <pedrolcrisp@gmail.com>
2023-10-30 16:15:02 -0300

    dark theme finalizado

---
+++ src/app/core/components/header/header.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { ETheme } from '../../enums/ETheme.enum';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent {
+
+  public icon: string = ETheme.ICON_MOON;
+  public textTheme: string = ETheme.TEXT_MOON;
+
+  public toggle(){
+    const theme = document.body.classList.toggle('dark-theme');
+
+    if(theme){
+      this.textTheme = ETheme.TEXT_SUN;
+      return (this.icon = ETheme.ICON_SUN);
+    }
+    this.textTheme = ETheme.TEXT_MOON;
+    return (this.icon = ETheme.ICON_MOON);
+  }
+
+}
